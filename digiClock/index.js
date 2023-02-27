c5d11c89f5d43abb56f309c7bbc5ebb44f268ded
charlie-s1 <charlie.d.smith98@gmail.com>
2023-02-27 13:02:29 +0000

    add 0 to clock if single digit

--- digiClock/index.js
+++ digiClock/index.js
@@ -4,18 +4,17 @@ window.addEventListener("load",()=>{
 });
 
 function main(){
-    console.log("refresh");
     const time = new Date();
     const clockCont = document.querySelector("#clockCont");
     clockCont.innerHTML = "";
 
     const hours = document.createElement("h1");
     hours.id = "hrs"
-    hours.textContent = time.getHours();
+    hours.textContent = time.getHours()<10 ? "0"+time.getHours() : time.getHours();
 
     const mins = document.createElement("h1");
     mins.id = "mins"
-    mins.textContent = time.getMinutes();
+    mins.textContent = time.getMinutes()<10 ? "0"+time.getMinutes() : time.getMinutes();
 
     clockCont.appendChild(hours);
     clockCont.appendChild(mins);    
